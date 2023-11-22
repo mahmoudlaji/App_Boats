16f11b5d358753c38ad44466b271891248408639
Dalina LESOURD <dalinaram@gmail.com>
2023-11-22 16:10:41 +0100

    updates

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,18 +1,22 @@
-# app/controllers/boats_controller.rb
 class BoatsController < ApplicationController
-  before_action :set_boat, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @boats = Boat.all
-  end
+    before_action :set_boat, only: [:show, :edit, :update, :destroy]
 
-  def new
-    @boat = Boat.new
-  end
+    def index
+      if params[:query].present?
+        @query = params[:query]
+        @boats = Boat.where("name LIKE ?", "%#{params[:query]}%")
+        # Preventing SQL Injection and Database error for
+        # unknown characters
+      else
+      @boats = Boat.all
+      end
+    end
 
-  def show
-  end
 
+    def new
+      @boat = Boat.new
+    end
 
     def create
       @boat = Boat.new(boat_params)
@@ -23,58 +27,31 @@ class BoatsController < ApplicationController
       end
     end
 
-
-    if @boat.save
-      redirect_to boat_path(@boat)
-    else
-      render :new, status: :unprocessable_entity
+    def show
     end
-  end
 
-  def edit
-  end
-
-  def update
-    if @boat.update(boat_params)
-      redirect_to boat_path(@boat)
-    else
-      render :edit, status: :unprocessable_entity
+    def edit
     end
-  end
 
-  def destroy
-    @boat.destroy
-    redirect_to boats_path
-  end
+    def  update
+      if @boat.update(boat_params)
+        redirect_to boat_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
 
-  def search
-    @boats = if params[:query].present?
-               perform_search(params[:query])
-             else
-               Boat.all
-             end
-    render :index
-  end
-  private
+    def destroy
+      @boat.destroy
+      redirect_to boats_path
+    end
 
-  def set_boat
-    @boat = Boat.find_by(id: params[:id])
-
+    private
+    def set_boat
+     @boat = Boat.find(params[:id])
+    end
 
     def boat_params
       params.require(:boat).permit(:category, :name, :address, :description, :price, :photo, :photos)
-
-    unless @boat
-      flash[:alert] = "Boat not found"
-      redirect_to root_path
-
     end
-  end
-
- 
-
-  def perform_search(query)
-    category_query = query["category"].downcase if query.present? && query["category"].present?
-    Boat.where("lower(category) = ?", category_query)
-  end
 end
