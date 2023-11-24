e2aaf9014d7bd6163948315ddd92355dc6c30d94
lajimi mahmoud <mahmoudladjimi@gmail.com>
2023-11-24 11:59:55 +0100

    form style

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_140307) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_boats_on_user_id"
